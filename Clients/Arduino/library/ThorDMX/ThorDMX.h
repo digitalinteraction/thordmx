91a2832f19e47373bbd13f84207069cd0ac877ed
tombartindale_cp <SND\tombartindale_cp>
2013-03-26 15:58:22 +0000

    Initial arduino import

---
+++ Clients/Arduino/library/ThorDMX/ThorDMX.h
@@ -0,0 +1,13 @@
+/*
+  ThorDMX.h - Library for communicating with a ThorDMX (Interaction Production) Server.
+  More information at ips.codeplex.com.
+  Created by Tom Bartindale, March 13, 2013.
+  Released into the public domain.
+*/
+#ifndef ThorDMX_h
+#define ThorDMX_h
+#include <stdlib.h>
+#include "DMX.h"
+
+DMX ThorDMX;
+#endif
